a7eebd5319afa89ef5f4367bc45689ffbfd9da01
Besher Al Maleh <besher.almaleh@mail.mcgill.ca>
2018-01-27 00:13:09 -0500

    Initial Commit

---
+++ Rain Check/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rain Check
+//
+//  Created by Besher on 2018-01-27.
+//  Copyright © 2018 Besher Al Maleh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
