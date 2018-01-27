db0ee3ce04dc927932fca3185ea94e18d2ff8c7d
Besher Al Maleh <besher.almaleh@mail.mcgill.ca>
2018-01-27 01:30:44 -0500

    complete basic weather functionality and city lookup

--- Rain Check/Controller/ChangeCityViewController.swift
+++ Rain Check/Controller/ChangeCityViewController.swift
@@ -9,28 +9,35 @@
 import UIKit
 import SVProgressHUD
 
-class ChangeCityViewController: UIViewController {
+protocol ChangeCityDelegate {
+    func userEnteredNewCity (city: String)
+}
 
+class ChangeCityViewController: UIViewController {
+    @IBOutlet weak var cityField: UITextField!
+    var delegate: ChangeCityDelegate?
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        SVProgressHUD.setBackgroundColor(UIColor.white)
+        SVProgressHUD.setDefaultMaskType(.gradient)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func checkWeather(_ sender: Any) {
+        
+        let cityName = cityField.text!
+        delegate?.userEnteredNewCity(city: cityName)
+        SVProgressHUD.show()
+        dismiss(animated: true) {
+            SVProgressHUD.dismiss()
+        }
+        
+        
     }
-    */
+    
+    
+    @IBAction func backButton(_ sender: Any) {
+        dismiss(animated: true)
+    }
 
 }
