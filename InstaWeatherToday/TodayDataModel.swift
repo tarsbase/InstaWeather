769bc519cfedeb8fb3d51f7e2d1a486ad57a5f04
BesherAlMaleh <besher.m@gmail.com>
2018-05-22 22:44:42 -0400

    hook up Dark Sky API

---
+++ InstaWeatherToday/TodayDataModel.swift
@@ -0,0 +1,15 @@
+//
+//  TodayDataModel.swift
+//  InstaWeatherToday
+//
+//  Created by Besher on 2018-05-22.
+//  Copyright © 2018 Besher Al Maleh. All rights reserved.
+//
+
+import Foundation
+
+struct TodayDataModel {
+    
+    
+    
+}
--- InstaWeatherToday/TodayViewController.swift
+++ InstaWeatherToday/TodayViewController.swift
@@ -16,7 +16,7 @@ class TodayViewController: UIViewController, NCWidgetProviding, CLLocationManage
     @IBOutlet weak var conditionImage: UIImageView!
     
     let locationManager = CLLocationManager()
-    var weatherDataModel = WeatherDataModel()
+    var todayModel = TodayDataModel()
     
     override func viewWillAppear(_ animated: Bool) {
         assignDelegate()
@@ -25,13 +25,18 @@ class TodayViewController: UIViewController, NCWidgetProviding, CLLocationManage
         updateData()
     }
     
+    // TODO : save weather data for next ViewDidLoad temporarily before connection
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         conditionImage.image = conditionImage.image?.withRenderingMode(.alwaysTemplate)
         conditionImage.tintColor = .darkGray
         
-        
+        let urlBegin = "https://api.darksky.net/forecast/"
+        let coords = "45.5329436,-73.5742773"
+        let url = urlBegin + DARK_SKY + coords
+        print(url)
         
         // Do any additional setup after loading the view from its nib.
     }
@@ -39,6 +44,30 @@ class TodayViewController: UIViewController, NCWidgetProviding, CLLocationManage
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func updateTodayModel() {
+        var test = "five"
+        print(test)
+        let urlBegin = "https://api.darksky.net/forecast/"
+        let coords = "45.5329436,-73.5742773"
+        let url = urlBegin + DARK_SKY + coords
+        print(url)
+        
+        Alamofire.request(url, method: .get, parameters: nil).responseJSON {
+            [unowned self] response in
+            if response.result.isSuccess {
+                let weatherJSON = JSON(response.result.value!)
+                print("SUCCESS")
+                print(weatherJSON)
+                print(" ")
+            } else {
+                print(response.result.error?.localizedDescription)
+                
+                
+                print("ERROR")
+            }
+        }
     }
     
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
@@ -57,6 +86,26 @@ class TodayViewController: UIViewController, NCWidgetProviding, CLLocationManage
     
     func updateData() {
         locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count - 1]
+        if location.horizontalAccuracy > 0 {
+            locationManager.stopUpdatingLocation()
+            locationManager.delegate = nil // prevents multiple refreshes
+            print("SUCCESS AT RETRIEVING LOCATION!!!!")
+            let latitude = String(location.coordinate.latitude)
+            let longitude = String(location.coordinate.longitude)
+            let params = ["lat": latitude, "lon": longitude, "appid": APP_ID]
+//            getWeatherData(url: weatherDataModel.weatherURL, parameters: params)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        let ac = UIAlertController(title: "Failed to retrieve location", message: "Your location is unkown", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Ok", style: .default))
+        present(ac, animated: true)
+//        cityLabel.text = "Location unavailable"
     }
     
 }
